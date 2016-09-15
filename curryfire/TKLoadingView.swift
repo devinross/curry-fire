43a556c6eb12fb71244087f56fd0f5fdc10f0882
Devin Ross <me@devinross.com>
2016-09-15 11:01:30 -0400

    Updating license text for some files.

--- curryfire/TKLoadingView.swift
+++ curryfire/TKLoadingView.swift
@@ -1,17 +1,39 @@
 //
 //  TKLoadingView.swift
-//  curryfire
-//
 //  Created by Devin Ross on 9/15/16.
-//  Copyright © 2016 Devin Ross. All rights reserved.
 //
+/*
+
+curryfire || https://github.com/devinross/curry-fire
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+*/
 
 import UIKit
 
 public class TKLoadingView: UIView {
 
 
-	
 	var loadingLabel : UILabel
 	var animating : Bool
 	
@@ -50,6 +72,16 @@ public class TKLoadingView: UIView {
 		
 	}
 	
+	override public func didMoveToWindow() {
+		
+		if self.animating {
+			NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(animateLabel), object: nil)
+			self.perform(#selector(animateLabel), with: nil, afterDelay: 0.25)
+		}
+		
+	}
+	
+	
 	public func startAnimating(){
 		
 		self.loadingLabel.alpha = 1
@@ -73,7 +105,7 @@ public class TKLoadingView: UIView {
 	
 	func animateLabel(){
 		
-				
+		
 		if(self.loadingLabel.superview == nil || !self.animating){
 			return
 		}
@@ -95,14 +127,7 @@ public class TKLoadingView: UIView {
 		
 	}
 	
-	override public func didMoveToWindow() {
 
-		if self.animating {
-			NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(animateLabel), object: nil)
-			self.perform(#selector(animateLabel), with: nil, afterDelay: 0.25)
-		}
-		
-	}
 	
 
 	
